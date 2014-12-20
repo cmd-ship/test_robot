43259e06a7ebde88723b5a18c882a2f8786c8b26
Robin Lockwood <robinlockw@gmail.com>
2014-12-19 17:44:27 -0700

    initial commit!  good job!

---
+++ Robot/src/Robot.cpp
@@ -0,0 +1,42 @@
+//============================================================================
+// Name        : Robot.cpp
+// Author      : Robin
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string drive();
+string turnLeft();
+string turnRight();
+string reverse();
+string shoot();
+
+int main() {
+	return 0;
+}
+
+string drive() {
+	return ""
+}
+
+string turnLeft() {
+	return ""
+}
+
+string turnRight() {
+	return ""
+}
+
+string reverse() {
+	return ""
+}
+
+string shoot() {
+	return ""
+}
